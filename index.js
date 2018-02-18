e19ed15cc65ea1fee5e2c3be4a2df2d8bb4d28f2
Jannis R <mail@jannisr.de>
2018-02-18 12:33:16 +0100

    implementation :sparkles:, tests :white_check_mark:

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict'
 
-// todo
+const byVal = (a, b) => a[1] - b[1]
+
+const record = (model) => {
+	// todo: use a Map for this?
+	const l = model.length
+	let map = new Array(l)
+	for (let i = 0; i < l; i++) map[i] = [i, model[i]]
+	map = map.sort(byVal)
+	for (let i = 0; i < l; i++) map[i] = map[i][0]
+
+	const apply = (arr) => {
+		const newArr = []
+		for (let i = 0; i < l; i++) newArr[i] = arr[map[i]]
+		return newArr
+	}
+	return apply
+}
+
+module.exports = record
--- test.js
+++ test.js
@@ -2,7 +2,28 @@
 
 const test = require('tape')
 
-const todo = require('.')
+const record = require('.')
 
-test('todo', (t) => {
+test('works', (t) => {
+	const apply = record([3, 1, 4, 2])
+	t.plan(3)
+
+	const a1 = apply(['c', 'a', 'd', 'b'])
+	t.deepEqual(a1, ['a', 'b', 'c', 'd'])
+	const a2 = apply([3, 1, 4, 2])
+	t.deepEqual(a2, [1, 2, 3, 4])
+	const a3 = apply(['3', '1', '4', '2'])
+	t.deepEqual(a3, ['1', '2', '3', '4'])
+})
+
+test('applies to unsortable arrays', (t) => {
+	const o1 = {}
+	const o2 = {}
+	const o3 = {}
+	const o4 = {}
+	t.plan(1)
+
+	const apply = record([3, 1, 4, 2])
+	const newArr = apply([o3, o1, o4, o2])
+	t.deepEqual(newArr, [o1, o2, o3, o4])
 })
